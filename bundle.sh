2902309613597a99b740c59a06d998020464f6a5
Carles Escrig Royo <esroyo@gmail.com>
2024-06-07 22:46:40 +0200

    fix: patch memory leak on webc

--- bundle.sh
+++ bundle.sh
@@ -3,3 +3,4 @@
 rm -rf dist/*
 npx rollup -c rollup.config.js
 sed -i '1s@^@import { Buffer } from "node:buffer"; import process from "node:process";@' dist/index.js
+sed -i 's/return this.getDigest(\([^)]*\))/\0;\1.end()/g' dist/index.js
