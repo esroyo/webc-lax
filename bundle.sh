e5bc2c421ed31e54e67f2b5f12740cdd0c6e347d
Carles Escrig Royo <esroyo@gmail.com>
2024-09-07 12:56:20 +0200

    chore: unify deps install with npm

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf dist/*
-npx rollup -c rollup.config.js
+deno run -A npm:rollup -c rollup.config.js
 sed -i '1s@^@import { Buffer } from "node:buffer"; import process from "node:process";@' dist/index.js
